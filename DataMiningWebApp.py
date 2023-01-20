e1f6b2f7b849c125b639fd29dd50ffedf256c472
Rattyba <86424584+Rattyba@users.noreply.github.com>
2023-01-20 19:22:03 +0800

    Update DataMiningWebApp.py

--- DataMiningWebApp.py
+++ DataMiningWebApp.py
@@ -9,7 +9,7 @@ st.title ("Data Science Fundementals Project: Movie Recommendation System")
 df = pd.read_csv("Cleaned_Dataset.csv")
 
 #Select Location
-Select = st.radio("Please select", ( "Introduction", "Question 2", "Question 3"))
+Select = st.radio("Please select", ( "Introduction", "Data Preprocessing", "Question 3"))
 
 
 if Select == "Introduction":
@@ -20,14 +20,12 @@ if Select == "Introduction":
     st.markdown('In this area of moderation, with the rapid development of technology, there is a big impact on people life, especially the changes of entertainment. Entertainment is  known as one of the parts of human. Different type of entertainment got different generation. Start from 19th century, as the development of movies, there are many types of movies occur in different generation. However, not every movie is recommended to watch, because different people got different preference. In this project, I aim to use a machine learning model which is k-means to categorize each movie based on their publish year. And from here find out which year range has the highest score. K-means is one of the clustering technics which can be categorized the item based on their characteristics.')
     
   
-elif Select == "Question 2":
+elif Select == "Data Preprocessing":
     # set header
-    st.header('Question 2: Which generation of customer spend the most time in the shop?')
+    st.header('Data Preprocessing')
     
-    # set subheader
-    st.subheader('Exploratory Data Analysis')
-    st.markdown('First, visualize the relationship of the sales and the weather.')
-    st.markdown('The first plot will be showing the sales during no rain day.')
+   
+    st.markdown('The dataset which I used for this project was get from Kaggle. The data name is movie.csv. There are 15 columns at this dataset. Which are name (String), rating (String), genre (String), year (Integer), released (String), score (float), votes (float), director (String), writer (String), star (String), country (String), budget (float), gross (float), company (String), runtime (float). But not every column will be used. I drop some unrelated columns, which are name, budget, gross, runtime, writer, star, country, company, director, rating.  After checking the whole dataset, there are some null data. So, for numerical data, I  used 0 to replace the empty value, and for categorical data, I used the word unknown to replace the empty data. ')
     image = Image.open('Sales vs Date (No Rain).jpg')
     st.image(image, caption='Sales vs Date (No Rain)')
     
